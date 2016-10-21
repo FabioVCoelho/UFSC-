457a037b04e4991ed8ba8cd2f16a4eb492434943
FabioVCoelho <angrazito@gmail.com>
2016-10-21 11:42:08 -0200

    DeadLock resolvido

--- Concorrente/Lab6/masterSlave.c
+++ Concorrente/Lab6/masterSlave.c
@@ -9,23 +9,20 @@ int main(int argc, char **argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	if (rank == 0)	{
-		for(i = 0; i < size; i++)	{
+		for(i = 1; i < size; i++)	{
 			MPI_Send(NULL,0,MPI_INT,i,0,MPI_COMM_WORLD);
 			printf("Sending from %d \n", rank);
 		}
-		for(i = 0; i < size; i++)	{
+		for(i = 1; i < size; i++)	{
 			int rankRecebido;
 			MPI_Recv(&rankRecebido,1,MPI_INT,i,MPI_ANY_TAG,MPI_COMM_WORLD,&st);
-			printf("Recieving from %d \n", rankRecebido);
+			printf("receiving from %d \n", rankRecebido);
 		}
 	} else {
-		for(i = 0; i < size; i++)	{
 			MPI_Recv(NULL,0,MPI_INT,0,MPI_ANY_TAG,MPI_COMM_WORLD,&st);
 			MPI_Send(&rank,1,MPI_INT,0,0,MPI_COMM_WORLD);
-		}
 	}
 	MPI_Finalize();
 	return 0;
 }
-
 
